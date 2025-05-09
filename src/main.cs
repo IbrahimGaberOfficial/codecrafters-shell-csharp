5cb8169cca5f2a6e565c2eb0e94b4aff236aa027
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2025-05-09 03:10:47 +0300

    move from old code to this new project, to bakc to specific state for tasks

--- src/main.cs
+++ src/main.cs
@@ -1,8 +1,111 @@
-using System.Net;
-using System.Net.Sockets;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
 
-// Uncomment this line to pass the first stage
-// Console.Write("$ ");
+public static class Program
+{
+    static string PATH = Environment.GetEnvironmentVariable("PATH");
 
-// Wait for user input
-Console.ReadLine();
+    public static int Main(string[] args)
+    {
+        while (true)
+        {
+            // Wait for user input
+            Console.Write("$ ");
+            var command = Console.ReadLine();
+            
+            if (string.IsNullOrEmpty(command))
+                continue;
+                
+            if (command == "exit 0")
+            {
+                return 0;
+            }
+            else if (command.StartsWith("echo "))
+            {
+                Console.WriteLine(command.Substring("echo ".Length));
+            }
+            else if (command.StartsWith("type"))
+            {
+                CheckType(command.Substring("type ".Length).Trim());
+            }
+            else
+            {
+                // Just defer to the shell to execute the command
+                // This will solve the issue with argv[0]
+                RunProgramUsingShell(command);
+            }
+        }
+    }
+    public static void printCommandNotFound(string command)
+    {
+        Console.WriteLine($"{command}: command not found");
+    }
+
+    private static void RunProgramUsingShell(string command)
+    {
+        var commandParts = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        var commandPath = FindExecutable(commandParts[0]);
+
+        if (commandPath == null){
+            printCommandNotFound(commandParts[0]);
+            return;
+        }
+
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = commandPath,
+            Arguments = command.Substring(commandParts[0].Length).Trim(),
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
+        };
+        
+        var process = new Process { StartInfo = startInfo };
+        process.Start();
+        
+        // // Read output
+         string output = process.StandardOutput.ReadToEnd();
+         string error = process.StandardError.ReadToEnd();
+        
+        Console.Write(output);
+        if (!string.IsNullOrEmpty(error))
+            printCommandNotFound(command);
+        //     Console.Error.Write(error);
+            
+        process.WaitForExit();
+    }
+
+    public static void CheckType(string command)
+    {
+        string[] builtInCommands = { "echo", "exit", "type", "pwd"};
+        if (builtInCommands.Contains(command))
+        {
+            Console.WriteLine($"{command} is a shell builtin");
+            return;
+        }
+        string result = FindExecutable(command);
+        if (result != null)
+        {
+            System.Console.WriteLine($"{command} is {result}");
+        }
+        else
+        {
+            Console.WriteLine($"{command}: not found");
+        }
+    }
+
+    private static string FindExecutable(string command)
+    {
+        string[] paths = PATH.Split(":", StringSplitOptions.RemoveEmptyEntries);
+        foreach (var path in paths)
+        {
+            string fullPath = Path.Combine(path, command);
+            if (File.Exists(fullPath))
+            {
+                return fullPath;
+            }
+        }
+        return null;
+    }
+}
